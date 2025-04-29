56212a6180a7282412c0d1a96d5bb1e4f080bc73
neptunia <kevinshen100@gmail.com>
2025-04-28 23:43:30 -0700

    readd prisma

---
+++ prisma/migrations/20230911000425_remove_username_usertoken/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `UserToken` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `UserToken` DROP COLUMN `userId`;
