4d9d2e8cd9386e999bb9a7d6d4a6212f4e45e3f3
neptunia <kevinshen100@gmail.com>
2026-01-20 22:48:41 -0500

    ok fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,9 +10,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter({
-			out: 'build'
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.env.CALMOE_BASE_URL || ''
 		}
